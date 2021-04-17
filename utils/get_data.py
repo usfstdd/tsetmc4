1ef9b425283b123963ac52e7b32303ba7f267a32
usfstd <usf.stdd@gmail.com>
2021-04-17 22:04:54 +0430

    delete numpy version of window function

--- utils/get_data.py
+++ utils/get_data.py
@@ -16,13 +16,7 @@ def get_price_df(share_name):
 
         return df
 
-def get_data(share_name):
-    df_inco = get_InCo_df(share_name)
-    df_price = get_price_df(share_name)
 
-    df_data = df_price.join(df_inco, how='inner')
-
-    return df_data
 
 
 
@@ -37,40 +31,8 @@ array([[1050.],
        [4310.]])
 '''
 
-def window(data, window_len):
-    '''
-    convert input data with
-    shape (sample_count, features) to 
-    shape (:, time_step, n_features)
-    '''
-
-    m = len(data)-(window_len-1)
-    n_features = len(data[0])
-
-    X = np.zeros(shape=(m,window_len,n_features))
-
-    for i in range(window_len):
-            X[:,i,:] = data[i:i+m,:]
-
-    return X
-# prepaire y for a sequence to sequence model :
 
 
 
 
-def windowXY(data, n_time_steps, n_steps_ahead, target_feature=0):
-    
-    print(f'data is:\n{data}')
-    X = window(data,n_time_steps)
-    print(f'X is:\n{X}')
-    
 
-
-    m = len(X) - (n_steps_ahead)
-    Y = np.zeros(shape=(m, n_time_steps, n_steps_ahead))
-    
-    for j in range(n_steps_ahead):
-        for i in range(n_time_steps):
-            Y[:,i, j] = data[(i+j+1):(i+j+1)+m, target_feature]
-
-    return X[:len(Y)], Y
